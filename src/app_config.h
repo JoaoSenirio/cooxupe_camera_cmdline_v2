8abaa6ceb4a8088f491138e817376583f5d2dcbe
joaosenirio <senirio10@gmail.com>
2026-03-18 13:55:34 -0300

    [Change] Mudando configuração para fx17

--- src/app_config.h
+++ src/app_config.h
@@ -28,16 +28,16 @@ struct AppConfig {
 inline AppConfig MakeDefaultConfig() {
     AppConfig config{};
     config.license_path = L"C:/Users/Public/Documents/Specim/SpecSensor.lic";
-    config.device_index = 10;
-    config.exposure_ms = 4.0;
-    config.frame_rate_hz = 120.0;
+    config.device_index = 5;
+    config.exposure_ms = 6.5;
+    config.frame_rate_hz = 150.0;
     config.binning_spatial = 2;
     config.binning_spectral = 1;
     config.calibration_scp_path = L"E:/Calibrations/3210495_20220310_calpack.scp";
     config.output_dir = "C:/SpecSensor/output";
-    config.rgb_wavelength_nm[0] = 610;
-    config.rgb_wavelength_nm[1] = 534;
-    config.rgb_wavelength_nm[2] = 470;
+    config.rgb_wavelength_nm[0] = 1039;
+    config.rgb_wavelength_nm[1] = 1370;
+    config.rgb_wavelength_nm[2] = 1625;
     config.capture_seconds = 40;
     config.dark_frames = 50;
     config.wait_timeout_ms = 1000;
@@ -45,7 +45,7 @@ inline AppConfig MakeDefaultConfig() {
     config.save_queue_capacity = 200;
     config.save_block_frames = 64;
     config.save_queue_push_timeout_ms = 2000;
-    config.camera_name = "FX10";
+    config.camera_name = "FX17";
     config.pipe_name = "\\\\.\\pipe\\specsensor_sample_pipe";
     config.log_file_path = "C:/SpecimOutput/specsensor_cli.log";
     return config;
